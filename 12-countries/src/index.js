ff4a835fcf4eb31a2fdeae0d51489ab31535d2b5
KalinkinaD <dkalinina8@gmail.com>
2020-06-28 17:04:20 +0300

    fixed

--- 12-countries/src/index.js
+++ 12-countries/src/index.js
@@ -9,6 +9,9 @@ import country from './templates/country.hbs';
 let inputText = document.querySelector('.input_text');
 
 let search = async () => {
+  if (inputText.value === '') {
+    return;
+  }
   let result = await fetchCountries(inputText.value);
   document.querySelector('.js-result').innerHTML = '';
   if (!result) {
@@ -23,14 +26,9 @@ let search = async () => {
     document.querySelector('.js-result').innerHTML = countryList(result);
   }
 };
-const showError = text => {
-  error({
-    text: text,
-    closer: false,
-    sticker: false,
-    destroy: true,
-    delay: 500,
-  });
-};
-
 inputText.addEventListener('input', debounce(search, 500));
+window.addEventListener('keypress', event => {
+  if (event.keyCode == 13) {
+    event.preventDefault();
+  }
+});
